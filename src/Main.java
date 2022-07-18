653048eecd990007bdbbd3ce98421d2fee769476
Серков Евгений <exzojs@gmail.com>
2022-07-18 22:12:39 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int client = 200;
-        int summa = 900;
+        int summa = 1001;
         int bonus = summa / 100 + summa;
         int bonusRub = summa / 100;
-        int bezBonusa = summa;
-        int bezDeneg = 0;
         boolean sBonusom = summa >= 1000;
-        int itog = sBonusom ? bonus : bezBonusa;
-        int bonusShow = sBonusom ? bonusRub : bezDeneg;
+        int itog = sBonusom ? bonus : summa;
+        int bonusShow = sBonusom ? bonusRub : 0;
         System.out.println(itog + client + " Итоговый счет");
         System.out.println(bonusShow + " Бонусные рубли");
 
